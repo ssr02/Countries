ca2d203adc81863238a8b11bf7d37c18fa5cc467
ssr02 <88631549+ssr02@users.noreply.github.com>
2021-08-15 13:17:57 +0530

    Add files via upload

---
+++ js/details.js
@@ -0,0 +1,111 @@
+"use strict";
+
+const btnBack = document.querySelector(".back");
+const themeSwitchBtn = document.querySelector("#theme__switch");
+const mainWrapper = document.querySelector("main");
+const flagContainer = document.querySelector(".container-flag");
+const descriptionContainer = document.querySelector(".container-desc");
+const neighbourCountries = document.querySelector(".surround");
+
+//------------------ Switch theme funtionality --------------------
+themeSwitchBtn.addEventListener("click", function (e) {
+  const moon = e.target.closest(".theme__switch").firstElementChild;
+
+  //change the moon icon
+  moon.classList.toggle("bi-moon");
+  moon.classList.toggle("bi-moon-fill");
+
+  //add dark theme class
+  document.body.classList.toggle("dark-theme");
+});
+
+//going back to main page
+btnBack.addEventListener("click", function () {
+  window.location.pathname = `/index.html`;
+});
+
+//-----------render country data ---------------------
+const renderCountryFlag = function (data) {
+  const htmlEl = `
+  <img src="${data.flag}" alt="flag" srcset="" class="flag">
+  `;
+
+  flagContainer.insertAdjacentHTML("afterbegin", htmlEl);
+};
+
+const renderCountryDescription = function (data) {
+  const htmlEl = `<h3 class="country-title">${data.name}</h3>
+  <div class="sect des1">
+      <p>Native Name:<span> ${data.nativeName}</span></p>
+      <p>Population:<span> ${Intl.NumberFormat(navigator.locale).format(
+        data.population
+      )}</span></p>
+      <p>Region:<span> ${data.region}</span></p>
+      <p>Sub Region:<span> ${data.subregion}</span></p>
+      <p>Capital:<span> ${data.capital}</span></p>
+  </div>
+  <div class="sect des2">
+      <p>Top Level Domain:<span> ${data.topLevelDomain}</span></p>
+      <p>Currencies:<span> ${data.currencies[0].name} (${
+    data.currencies[0].symbol
+  })</span></p>
+      <p>Languages:<span> ${arrayToString(data.languages)}</span></p>
+  </div>`;
+
+  descriptionContainer.insertAdjacentHTML("afterbegin", htmlEl);
+};
+
+const renderBorders = function (borderArr) {
+  if (borderArr.length === 0) {
+    const noBorders = `<button class="btn country-btn">
+    0 Neighbouring Countries</button>`;
+    neighbourCountries.insertAdjacentHTML("afterbegin", noBorders);
+  }
+
+  borderArr.forEach((border) => {
+    fetch(`https://restcountries.eu/rest/v2/alpha/${border}`)
+      .then((res) => res.json())
+      .then((data) => {
+        const htmlEl = `<button class="btn country-btn">
+                        ${data.name}</button>`;
+        neighbourCountries.insertAdjacentHTML("afterbegin", htmlEl);
+      })
+      .catch((err) => console.log(err));
+  });
+};
+
+//-------------------get country info ---------------------------
+
+let data;
+const getSelectedCountryData = async function () {
+  let countryName = JSON.parse(localStorage.getItem("cName"));
+  // console.log(JSON.parse(countryName));
+
+  if (countryName) {
+    const res = await fetch(
+      `https://restcountries.eu/rest/v2/name/${countryName}`
+    );
+    data = await res.json();
+
+    console.log(data);
+
+    data.forEach((c) => {
+      if (c.name === countryName) {
+        renderCountryFlag(c);
+        renderCountryDescription(c);
+        renderBorders(c.borders);
+      }
+    });
+  }
+};
+
+getSelectedCountryData();
+
+const arrayToString = function (arr) {
+  let langs = [];
+  arr.forEach((l) => {
+    langs.push(l.name);
+  });
+  langs = langs.join(", ");
+  return langs;
+};
---
+++ js/main.js
@@ -0,0 +1,120 @@
+"use strict";
+
+const btnDropdown = document.querySelector("#dropdownBtn");
+const regions = document.querySelector(".country-change");
+const chevronIcon = document.querySelector(".bi.bi-chevron-down");
+const themeSwitchBtn = document.querySelector("#theme__switch");
+const searchCountry = document.querySelector("#search");
+const countryContainer = document.querySelector(".countries");
+const form = document.querySelector(".form");
+
+window.addEventListener("load", function (e) {
+  getCountriesByRegion("europe");
+  searchCountry.focus();
+});
+
+const renderCountry = function (data) {
+  if (data) {
+    data.forEach((c) => {
+      countryContainer.insertAdjacentHTML(
+        "beforeend",
+        ` <div class="country flex" data-name="${c.name}">
+        <div class="part flag">
+            <img src="${c.flag}" alt="" class="flag">
+        </div>
+        <div class="part details">
+            <p class="country-name">${c.name}</p>
+            <p class="desc">Population: <span>${new Intl.NumberFormat(
+              navigator.language
+            ).format(c.population)}</span></p>
+            <p class="desc">Region: <span>${c.region}</span></p>
+            <p class="desc">Capital: <span>${c.capital}</span></p>
+        </div>
+    </div>`
+      );
+    });
+  }
+};
+
+//----------------fetch data from RestCountries API----------------
+let dataByName;
+let dataByRegion;
+
+// fetching countries by name
+const getCountriesByName = async function (countryName) {
+  if (countryName) {
+    const res = await fetch(
+      `https://restcountries.eu/rest/v2/name/${countryName}`
+    );
+    dataByName = await res.json();
+
+    renderCountry(dataByName);
+  }
+};
+
+// fetching countries by region
+const getCountriesByRegion = async function (region) {
+  if (region) {
+    const res = await fetch(
+      `https://restcountries.eu/rest/v2/region/${region}`
+    );
+    dataByRegion = await res.json();
+    renderCountry(dataByRegion);
+  }
+};
+
+//------------------sort/show countries by name------------------
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  let country = e.target.querySelector("#search").value;
+
+  // country = e.target.value;
+  // console.log(country);
+  countryContainer.innerHTML = "";
+  getCountriesByName(country);
+});
+
+//------------------sort/show countries by region------------------
+regions.addEventListener("click", function (e) {
+  if (e.target.classList.contains("region")) {
+    countryContainer.innerHTML = "";
+    getCountriesByRegion(e.target.dataset.region);
+    console.log(e.target.dataset.region);
+    btnDropdown.firstElementChild.textContent = `${e.target.textContent}`;
+  }
+});
+
+//------------------ Switch theme funtionality --------------------
+themeSwitchBtn.addEventListener("click", function (e) {
+  const moon = e.target.closest(".theme__switch").firstElementChild;
+
+  //change the moon icon
+  moon.classList.toggle("bi-moon");
+  moon.classList.toggle("bi-moon-fill");
+
+  //add dark theme class
+  document.body.classList.toggle("dark-theme");
+});
+
+//------------------dropdown mechanism------------------
+btnDropdown.addEventListener("click", function (e) {
+  e.preventDefault();
+  regions.classList.toggle("hidden");
+  chevronIcon.classList.toggle("rotate");
+});
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape") {
+    chevronIcon.classList.remove("rotate");
+    regions.classList.add("hidden");
+  }
+});
+
+countryContainer.addEventListener("click", function (e) {
+  // console.log(e.target.closest(".country").dataset.countryName);
+  let selectedCountry = e.target.closest(".country").dataset.name;
+  localStorage.setItem("cName", JSON.stringify(selectedCountry));
+  console.log(window.location);
+  window.location.pathname = `/details.html`;
+});
